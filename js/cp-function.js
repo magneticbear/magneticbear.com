dd78a212b1c8d1f56639f16f102c5345bf263b97
Mo Mozafarian <mo@magneticbear.com>
2012-11-28 15:15:55 -0500

    Updated Lab and CP

---
+++ js/cp-function.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+
+$(document).ready(function() {
+	
+	$(".stage").toggle(function(){$(this).siblings(".extra").css("height","auto");}, function(){$(this).siblings(".extra").css("height","0");})
+	$(".stage").toggle(function(){$(this).siblings(".extra").css("border-top","1px solid #CCC");}, function(){$(this).siblings(".extra").css("border","none");})	
+	
+	$('.icon').mouseenter(function() {
+  		$(this).children('.progress').css('height','140px');
+	});
+		
+		$('.icon').mouseleave(function() {
+  		$(this).children('.progress').css('height','10px');
+	});	
+	
+});	
+	
+
+		
+	
--- js/function.js
+++ js/function.js
@@ -2,14 +2,9 @@
 
 $(document).ready(function() {
 	
-	$(".expand").toggle(function(){$(this).parent(".lab article").css("height","auto");}, function(){$(this).parent(".lab article").css("height","20.5em");})
-	
-	$("article").toggle(function(){$(this).children(".extra").css("height","auto");}, function(){$(this).children(".extra").css("height","0");})
-	$("article").toggle(function(){$(this).children(".extra").css("border-top","1px solid #CCC");}, function(){$(this).children(".extra").css("border","none");})
+//	$("article").toggle(function(){$(this).children(".extra").css("height","auto");}, function(){$(this).children(".extra").css("height","0");})
+//	$("article").toggle(function(){$(this).children(".extra").css("border-top","1px solid #CCC");}, function(){$(this).children(".extra").css("border","none");})	
 
-	$(".expand").toggle(function(){$(this).css("background-image","url(images/minus-btn.png)");}, function(){$(this).css("background-image","url(images/plus-btn.png)");})
-	
-	
 	$(".menu").toggle(function(){$("html").css("margin-left","8em");}, function(){$("html").css("margin-left","0");})	
 	$(".menu").toggle(function(){$(".menu").css("background","#999 url(images/menu-icon-back.png) no-repeat center center");}, function(){$(".menu").css("background","#999 url(images/menu-icon.png) no-repeat center center");})	
 	$(".menu").click(function() {$('.menu').css('background-size','60%')});
@@ -24,6 +19,8 @@ $(document).ready(function() {
 		$('.avatar img').css('opacity','1');	
 	});
 	
+
+	
 	$('.icon').mouseenter(function() {
   		$(this).children('.progress').css('height','140px');
 	});
@@ -31,6 +28,7 @@ $(document).ready(function() {
 		$('.icon').mouseleave(function() {
   		$(this).children('.progress').css('height','10px');
 	});	
+	
 	
 	setTimeout(function (){	
 	$(".home li:nth-child(1)").css('right','-3%')}, 1600);
