77c3ac9bf3671165fcbd211e707227145d968755
JP Simard <jp@jpsim.com>
2012-11-30 17:30:55 -0500

    Readded original files

---
+++ original/js/function.js
@@ -0,0 +1,49 @@
+// JavaScript Document
+
+$(document).ready(function() {
+	
+//	$("article").toggle(function(){$(this).children(".extra").css("height","auto");}, function(){$(this).children(".extra").css("height","0");})
+//	$("article").toggle(function(){$(this).children(".extra").css("border-top","1px solid #CCC");}, function(){$(this).children(".extra").css("border","none");})	
+
+	$(".menu").toggle(function(){$("html").css("margin-left","8em");}, function(){$("html").css("margin-left","0");})	
+	$(".menu").toggle(function(){$(".menu").css("background","#999 url(images/menu-icon-back.png) no-repeat center center");}, function(){$(".menu").css("background","#999 url(images/menu-icon.png) no-repeat center center");})	
+	$(".menu").click(function() {$('.menu').css('background-size','60%')});
+	
+	$('.avatar').mouseenter(function() {
+		$(this).children('.avatar a').css('display','block');
+		$(this).children('.avatar img').css('opacity','0.5');
+	});
+	
+	$('.avatar').mouseleave(function() {
+		$('.avatar a').css('display','none');
+		$('.avatar img').css('opacity','1');	
+	});
+	
+
+	
+	$('.icon').mouseenter(function() {
+  		$(this).children('.progress').css('height','140px');
+	});
+		
+		$('.icon').mouseleave(function() {
+  		$(this).children('.progress').css('height','10px');
+	});	
+	
+	
+	setTimeout(function (){	
+	$(".home li:nth-child(1)").css('right','-3%')}, 1600);
+	setTimeout(function (){	
+	$(".home li:nth-child(2)").css('right','-3%')}, 1600);
+	setTimeout(function (){	
+	$(".home li:nth-child(3)").css('right','10.5%')}, 1600);
+	setTimeout(function (){	
+	$(".home li:nth-child(4)").css('right','27.5%')}, 1600);
+	setTimeout(function (){	
+	$(".home li:nth-child(5)").css('right','44%')}, 1600);
+	setTimeout(function (){	
+	$(".home li:nth-child(6)").css('right','60%')}, 1600);		
+});	
+	
+
+		
+	
