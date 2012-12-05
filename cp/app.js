83666715771eaaaec1ccd56b68df37c8b666e2b4
Adrian Seeley <adrian@gatosomina.com>
2012-12-05 09:54:27 -0500

    implementing cp hotswap

--- cp/app.js
+++ cp/app.js
@@ -1,87 +1,23 @@
-var port			= process.env.PORT || 80;
-var fs				= require('fs');
-var db_url			= process.env.MONGOHQ_URL || 'mbsweb';
-var db				= require('mongojs').connect(db_url, ['projects', 'log']);
-var express			= require('express');
-var app				= express();
+var port	 = process.env.PORT || 80;
+var fs		 = require('fs');
+var db_url	 = process.env.MONGOHQ_URL || 'mbsweb';
+var db		 = require('mongojs').connect(db_url, ['projects', 'log']);
+var express	 = require('express');
+var app		 = express();
 
-var html_global_prefix  = fs.readFileSync('html/global_prefix.html',  'utf8');
-var html_global_postfix = fs.readFileSync('html/global_postfix.html', 'utf8');
-var html_500            = '<html><b>500</b> DB IS DOWN!</html>';
-var html_404            = '<html><b>404</b> PAGE NOT FOUND!</html>';
+var html_cp  = fs.readFileSync('html/cp.html',  'utf8');
+var html_500 = '<html><b>500</b> DB IS DOWN!</html>';
+var html_404 = '<html><b>404</b> PAGE NOT FOUND!</html>';
 
 app.use(express.bodyParser());
 
 app.get('/cp/:project', serve_cp);
 
-db.projects.save(
-	{
-		project_url:  'test',
-
-		project_name: 'Test Project', 
-		
-		stage_completions: 
-		{
-			pre: 						 0.0,
-			design: 	 				 0.1,
-			development: 				 0.5,
-			testing: 					 0.3,
-			make_money_money_make_money: 1.0
-		},
-
-		feed:
-		[
-			{
-				date: 	  new Date().getTime(),
-				markdown: '## THIS SHOULD BE BIG',
-				tags:
-				[
-					'stage_pre',
-					'stage_design',
-					'mo@magneticbear.com'
-				]
-			},
-			{
-				date: 	  new Date().getTime() - 1000,
-				markdown: '#### THIS SHOULD BE LESS BIG',
-				tags:
-				[
-					'stage_pre',
-					'stage_design',
-					'mo@magneticbear.com'
-				]
-			},
-			{
-				date: 	  new Date().getTime() - 4000,
-				markdown: 'THIS SHOULD BE normal BIG',
-				tags:
-				[
-					'stage_pre',
-					'stage_design',
-					'mo@magneticbear.com'
-				]
-			},
-			{
-				date: 	  new Date().getTime() - 402000,
-				markdown: 'This markdown will automatically be converted to HTML whenever the page is served ;) ',
-				tags: 	  []
-			}
-		],
-
-		authorized_clients:
-		[
-			'adrian@magneticbear.com',
-			'mo@magneticbear.com',
-			'jp@magneticbear.com'
-		],
-
-		public_is_authorized: 1
-	}
-);
 
 function serve_cp(req, res)
 {
 	// todo: validate req.params.project before using
+
 	db.projects.findOne({project_url: req.params.project}, 
 		function(err, doc)
 		{
@@ -90,7 +26,9 @@ function serve_cp(req, res)
 			else if(!doc) serve(req, res, 404, html_404, new Date().getTime());
 			else
 			{
-				serve(req, res, 200, '<html>' + JSON.stringify(doc) + '</html>', new Date().getTime());
+				var content  = ''
+				var to_serve = html_cp.replace('{{content}}', content);
+				serve(req, res, 200, to_serve, new Date().getTime());
 			}
 		}
 	);
