b4ccbf7382b792b876af28b031283798190a34d4
Adrian Seeley <adrian@gatosomina.com>
2012-12-27 12:13:19 -0500

    cp cp cp

--- cp/app.js
+++ cp/app.js
@@ -1,7 +1,7 @@
 var port	 = process.env.PORT || 80;
 var fs		 = require('fs');
 var db_url	 = process.env.MONGOHQ_URL || 'mbsweb';
-var db		 = require('mongojs').connect(db_url, ['projects', 'log']);
+var db		 = require('mongojs').connect(db_url, ['users', 'projects', 'log']);
 var md 		 = require("node-markdown").Markdown;
 var express	 = require('express');
 var app		 = express();
@@ -18,22 +18,44 @@ app.get ('/login', 		   login);
 app.post('/process_login', process_login);
 
 
-function login(req, res)
+function login(req, res, failed)
 {	
 	res.status(200); res.setHeader('Content-Type', 'text/html');
 	res.end(HTML_login);
 }
 function process_login(req, res)
 {
+	db.users.findOne({email: req.body.email}, 
+		function (err, doc)
+		{
+			if (err)  	   { err  (req, res, err);  return; }
+			if (!doc) 	   { login(req, res, true); return; }
+			if (doc.admin) { admin(req, res); 		return; }
+		}
+	);
+
 	res.status(200); res.setHeader('Content-Type', 'text/html');
 	res.end(HTML_admin_panel);
 }
+function admin(req, res)
+{
+
+}
+function err(req, res, err)
+{
+
+}
 function now() { return new Date().getTime(); }
 
-db.projects.ensureIndex({project_url:1});
 
-//delete_project('test');
-//create_new_project('test', 'Test Project');
+db.users.remove();
+db.projects.remove();
+db.log.remove();
+
+db.users.ensureIndex({email:1});
+
+
+db.users.add({email: 'abs@mbs.com', password: 'foobar', admin:1});
 
 app.listen(port);
 console.log('msbweb serving: ' + port);
